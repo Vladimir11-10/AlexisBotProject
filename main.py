8938bb429af3c24199199e84c5a08c9772762750
pc <vladimirwork1110@gmail.com>
2026-03-10 19:59:38 +0300

    commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import sys
+import datetime as dt
 
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.client.default import DefaultBotProperties
@@ -9,21 +10,59 @@ from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, FSInputFile
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options # Подключаем все необходимые библиотеки
+
 
 API_TOKEN = "8702806640:AAHg8TOMEsOr0FsQzfLT2_VnVmM272DWz_s"
+ADMIN = '939104347'
 
 
 dp = Dispatcher()
 
 
-@dp.message(CommandStart())
+@dp.message(CommandStart()) # функция, обрабатывающая start - приветствие, краткое описание, список команд
 async def command_start_handler(message: Message) -> None:
     await message.answer(f'Здравствуйте, {message.from_user.first_name}! Здесь вы можете ознакомиться с одним из моих '
                          f'проектов - ботом Алексис. В нем представлено несколько демонстративных функций, которые вы'
                          f'можете опробовать.')
+    await Bot(token=API_TOKEN).send_message(text=f'{message.from_user.first_name} started bot. '
+                                                 f'{dt.datetime.now().strftime("%H:%M")}', chat_id=ADMIN)
     await message.answer('Ссылка на страничку на Kwork:')
     await message.answer('https://kwork.ru/user/_vladimir-')
+    await message.answer('Список актуальных команд для бота:\n/kwork_link - ссылка на Kwork'
+                         '\n/parse - парсинг курса валют в данный момент')
 
+
+@dp.message(Command('parse')) # парсинг сайта ЦБ РФ с курсом валют
+async def value_parsing(message: Message) -> None:
+
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options) # обращаемся к сайту, берем код html
+    driver.get("https://cbr.ru/curreNcy_base/daily/")
+
+    text = 'Цифр. код - Букв. код - Единиц - Валюта - Курс'
+    for i in driver.page_source.split('<tbody>')[2].split('</tbody>')[0].split('<tr>'):
+        if 'td' in i:
+            element = []
+            for j in i.split('</tr>')[0].split('</td>'):
+                if 'td' in j:
+                    element.append(j.split()[0][4:])
+            text += f'\n\n{' - '.join(element)}'
+    driver.quit()
+    await message.answer(text=text)
+    await message.answer('Если хотите проверить достоверность информации, можете зайти в источник по ссылке ниже.')
+    await message.answer('https://cbr.ru/curreNcy_base/daily/')
+
+
+@dp.message(Command('kwork_link')) # при желании еще раз даем ссылку на профиль на Kwork
+async def kwork_link(message: Message) -> None:
+    await message.answer('Ссылка на страничку на Kwork:')
+    await message.answer('https://kwork.ru/user/_vladimir-')
 
 # @dp.message(Command("show"))
 # async def show(message: types.Message):
